20326fddddf67d57956a34d0dbd6f08b1b5a92c2
Sona Shrestha <sona.shrestha004@gmail.com>
2021-08-05 20:26:20 +1000

    adding footer section

---
+++ client/src/pages/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer className="text-center text-lg-start bg-light text-muted">
+      <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div className="me-5 d-none d-lg-block">
+          <span>Get connected with us on social networks:</span>
+        </div>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
