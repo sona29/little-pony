825a4020a865e71d5515732605651589d04d0c0e
Sona Shrestha <sona.shrestha004@gmail.com>
2021-08-07 12:16:49 +1000

    adding validation

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -84,149 +84,164 @@ function DashBoard() {
   return (
     <>
       <div className="container my-1">
-        <Link to="/">← Back to Products</Link>
+        <div class="align-items-center">
+          <Link to="/">← Back to Products</Link>
 
-        {user ? (
-          <>
-            <h2>Product Dashboard</h2>
-            <form onSubmit={handleFormSubmit}>
-              <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Product Name</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="name"
-                  id="product-name"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter product name"
-                  onChange={handleChange}
-                ></input>
-              </div>
+          {user ? (
+            <>
+              <h2 className="text-center">Product Dashboard</h2>
+              <form
+                onSubmit={handleFormSubmit}
+                className="border-blue my-1 align-items-center"
+              >
+                <div className="form-group my-2">
+                  <label htmlFor="exampleInputEmail1">Product Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="name"
+                    id="product-name"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter product name"
+                    onChange={handleChange}
+                    required
+                  ></input>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">
-                  Product Description
-                </label>
-                <textarea
-                  className="form-control"
-                  id="product-description"
-                  rows="3"
-                  onChange={handleChange}
-                  name="description"
-                ></textarea>
-              </div>
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlTextarea1">
+                    Product Description
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="product-description"
+                    rows="3"
+                    onChange={handleChange}
+                    name="description"
+                    required
+                  ></textarea>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlSelect2">
-                  Product Condition
-                </label>
-                <select
-                  className="form-control"
-                  id="product-condition"
-                  onChange={handleChange}
-                  name="condition"
-                >
-                  <option>New</option>
-                  <option>Used (like-new)</option>
-                  <option>Used (good)</option>
-                  <option>Used (fair)</option>
-                </select>
-              </div>
-
-              {categories.data && (
-                <div className="form-group">
-                  <label htmlFor="exampleFormControlSelect2">Category</label>
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlSelect2">
+                    Product Condition
+                  </label>
                   <select
                     className="form-control"
                     id="product-condition"
-                    name="category"
                     onChange={handleChange}
+                    name="condition"
+                    required
                   >
-                    {categories.data.categories.map((category) => (
-                      <option key={category._id} value={category._id}>
-                        {category.name}
-                      </option>
-                    ))}
+                    <option>New</option>
+                    <option>Used (like-new)</option>
+                    <option>Used (good)</option>
+                    <option>Used (fair)</option>
                   </select>
                 </div>
-              )}
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">
-                  Product Color
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="color"
-                  id="product-quantity"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter product color"
-                  onChange={handleChange}
-                ></input>
-              </div>
+                {categories.data && (
+                  <div className="form-group my-2">
+                    <label htmlFor="exampleFormControlSelect2">Category</label>
+                    <select
+                      className="form-control"
+                      id="product-condition"
+                      name="category"
+                      onChange={handleChange}
+                      required
+                    >
+                      {categories.data.categories.map((category) => (
+                        <option key={category._id} value={category._id}>
+                          {category.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">
-                  Product Price
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="price"
-                  id="product-quantity"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter product color"
-                  onChange={handleChange}
-                ></input>
-              </div>
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlTextarea1">
+                    Product Color
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="color"
+                    id="product-quantity"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter product color"
+                    onChange={handleChange}
+                    required
+                  ></input>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">
-                  Product Size
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="size"
-                  id="product-quantity"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter product color"
-                  onChange={handleChange}
-                ></input>
-              </div>
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlTextarea1">
+                    Product Price
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="price"
+                    id="product-quantity"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter product color"
+                    onChange={handleChange}
+                    required
+                  ></input>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">Quantity</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  name="quantity"
-                  id="product-quantity"
-                  aria-describedby="emailHelp"
-                  placeholder="Enter product quantity"
-                  onChange={handleChange}
-                ></input>
-              </div>
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlTextarea1">
+                    Product Size
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="size"
+                    id="product-quantity"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter product color"
+                    onChange={handleChange}
+                    required
+                  ></input>
+                </div>
 
-              <div className="form-group">
-                <label htmlFor="exampleFormControlFile1">Upload Picture</label>
-                <input type="file" onChange={onChange} />
-                {/* {data && (
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlTextarea1">Quantity</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="quantity"
+                    id="product-quantity"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter product quantity"
+                    onChange={handleChange}
+                    required
+                  ></input>
+                </div>
+
+                <div className="form-group my-2">
+                  <label htmlFor="exampleFormControlFile1">
+                    Upload Picture
+                  </label>
+                  <input type="file" onChange={onChange} />
+                  {/* {data && (
                   <img
                     style={{ maxWidth: "100%" }}
                     src={data.upload.filename}
                     alt=""
                   />
                 )} */}
-              </div>
+                </div>
 
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </form>
-          </>
-        ) : null}
+                <button type="submit" className="btn btn-primary my-2">
+                  Add new product
+                </button>
+              </form>
+            </>
+          ) : null}
+        </div>
       </div>
     </>
   );
