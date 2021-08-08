20afdf9f84ab03a905f13b265b5796aa0f7927df
Sona Shrestha <sona.shrestha004@gmail.com>
2021-08-08 10:10:52 +1000

    updating change

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import unicorn from "../../assets/unicorn.png";
+import { BsPersonPlus } from "react-icons/bs";
+import { BsPersonCheck } from "react-icons/bs";
+import "./style.css";
 
 function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="navbar-nav">
-          <li className="nav-item">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item px-1">
             <Link to="/dashBoard">DashBoard</Link>
+          </li>
+          <li className="nav-item px-1">
             <Link to="/orderHistory">Order History</Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item px-1">
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
@@ -21,12 +27,16 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="navbar-nav">
-          <li className="nav-item px-2">
-            <Link to="/signup">Signup</Link>
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item px-2 access">
+            <Link to="/signup">
+              <BsPersonPlus /> Register
+            </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/login">Login</Link>
+          <li className="nav-item access">
+            <Link to="/login">
+              <BsPersonCheck /> Login
+            </Link>
           </li>
         </ul>
       );
@@ -35,14 +45,10 @@ function Nav() {
 
   return (
     <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">
-            🛍️
-          </span>
-          Little Pony
-        </Link>
-      </h1>
+      <Link to="/">
+        <img src={unicorn} alt="..." height="70" className="logo pt-1 pb-1" />
+        <span className="web-name">Little Pony</span>
+      </Link>
 
       <nav className="navbar navbar-expand-lg">
         <button
